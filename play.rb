e26dfd0c8510831539ce47961dfadfb37335f808
Chloe Chong <chloechong.us@gmail.com>
2020-11-16 22:18:37 -0500

    added some prompts

--- play.rb
+++ play.rb
@@ -1,4 +1,17 @@
 require_relative 'lib/board'
 
-board = Board.new(5)
-board.play!(10)
+p "Enter the size of the board (integer 1-20)"
+
+size = gets.chomp&.to_i
+
+if size > 20
+  p "Please enter an integer between 1-20"
+end
+
+size = gets.chomp&.to_i
+board = Board.new(size)
+
+p "Board created! How many moves do you want to see?"
+moves = gets.chomp&.to_i
+
+board.play!(moves)
